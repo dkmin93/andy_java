70c7135ef98bc6b05c0b495d88034f4f577818b9
Andy <dkmin9392@gmail.com>
2025-01-17 16:59:17 +0900

    17th

--- JavaQuiz/src/selfstudy/Notepad.java
+++ JavaQuiz/src/selfstudy/Notepad.java
@@ -6,26 +6,12 @@ public class Notepad {
 
 }
 
-
 class Solution {
-    public int solution(int[] sides) {
-        int answer = 0;
-        int fir = 0;
-        int sec = 0;
-        int thr = 1001;
-        for(int i = 0; i < sides.length; i++) {
-        	if(sides[i] > fir) {
-        		fir = sides[i];
-        	} else if(sides[i] < thr) {
-        		thr = sides[i];
-        	} else if(sides[i] > thr && sides[i] < fir) {
-        		sec = sides[i];
-        	}
-        }
-        if(sec+thr > fir) {
-        	answer = 1;
-        } else {
-        	answer = 2;
+    public int[] solution(String[] strlist) {
+        int[] answer = {};
+        for(int i = 0; i < strlist.length; i++) {
+        	
+        	answer[] = strlist[i].length();
         }
         return answer;
     }
